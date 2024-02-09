2bd8b47d7cac1796e891073e31bc08aacda99500
null-hackers <nullhackers79@gmail.com>
2024-02-10 01:03:21 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import NullhackersLogo from './assets/Null-logo.png';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+          <img src={NullhackersLogo} className="logo" alt="nullhackers logo" />
+      </div>
+      <h2>Nullhackers</h2>
+      <h1>Under Development</h1>
+      <p class="blink">We are Coming Soon</p>
+    </>
+  )
+}
+
+export default App
